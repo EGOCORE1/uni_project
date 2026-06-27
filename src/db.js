4778b64bea07438cd8f765cd405c7bcf0a68ec7e
Abdullah <a73898551@gmail.com>
2026-06-26 18:18:23 -0700

    add goals to db.js

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ import { events } from './models/event.js';
 import { registrations } from './models/registration.js';
 import { eventMedia } from './models/eventMedia.js';
 import { AboutUs } from './models/aboutUs.js';
+import { goals } from './models/goal.js';
 import * as relationsSchema from './models/relations.js';
 const client = createClient({ url: 'file:sqlite.db' });
 export const schema = { 
@@ -12,6 +13,7 @@ export const schema = {
     events, 
     registrations, 
     eventMedia, 
+    goals,
     AboutUs ,
     ...relationsSchema 
 };
